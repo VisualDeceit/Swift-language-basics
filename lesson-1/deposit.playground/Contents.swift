3211ab83398fe31a2b47055d7a60a7308bf3f2d4
Alexander Fomin <inzhener@outlook.com>
2020-10-16 13:41:01 +0300

    Added lesson-2 home work
    Some updates

--- lesson-1/deposit.playground/Contents.swift
+++ lesson-1/deposit.playground/Contents.swift
@@ -1,13 +1,14 @@
 import Foundation
 
 // процент
-let percent: Double = 15
+let percent: Double = 17.8
 //срок
-let yearsCount = 5
+let yearsCount = 12
 //сумма
-var amount: Double = 10000
+var amount: Double = 98280
 
 for _ in 1...yearsCount {
-    amount = amount * (1 + percent / 100)
+    amount = amount * (1 + percent / 100 / 12)
+    print(amount)
 }
 print("Сумма вклада через \(yearsCount) лет составит \(amount)")
